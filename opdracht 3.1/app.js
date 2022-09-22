b29a4fb19c289f9d0952a1f03c23824787a48323
Jesse-van-Vliet <9021012@student.zadkine.nl>
2022-09-22 10:44:07 +0200

    opdracht 3.1

---
+++ opdracht 3.1/app.js
@@ -0,0 +1,24 @@
+const myName ="Jesse";
+let myAge = 16;
+const diploma = true;
+let Passed;
+
+document.write(myName);
+document.write(myAge);
+document.write(diploma);
+document.write(Passed);
+
+let Num1 = 8;
+let Num2 = 9;
+let Answer = Num1 + Num2;
+console.log(Answer);
+
+
+let Answer2 = Num1 - Num2;
+console.log(Answer2);
+
+let Answer3 = Num1 * Num2;
+console.log(Answer3);
+
+let Answer4 = Num1 / Num2;
+console.log(Answer);

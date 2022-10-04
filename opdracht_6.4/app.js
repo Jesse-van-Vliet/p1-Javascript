a13d0aebe97785f3c665a3e6888b77433d5c9123
Jesse-van-Vliet <9021012@student.zadkine.nl>
2022-10-04 12:01:19 +0200

    opdracht_6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,33 @@
+function calculate(num, num2) {
+    let answer = num + num2;
+    console.log(answer)
+}
+
+calculate(8, 15)
+
+function multiplyByFive(num5) {
+answer = num5 * 5;
+console.log(answer)
+}
+
+multiplyByFive(5)
+
+function checkInput (num6, num7) {
+   if (num6>= num7) {
+    console.log(num6)
+
+   } else {
+    console.log(num7)
+   }
+
+}
+
+checkInput (9,20)
+
+
+
+function calculateMinutes (num3) {
+    let answer = num3 / 60;
+    console.log(answer)
+}
+calculateMinutes (3600)

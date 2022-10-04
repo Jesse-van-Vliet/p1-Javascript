fe20c363016118a400e457cce56366eb2bcefc45
Jesse-van-Vliet <9021012@student.zadkine.nl>
2022-10-04 10:59:34 +0200

    homework

---
+++ Opdracht_6.1/app.js
@@ -0,0 +1,7 @@
+function kleur_aanpassen () {
+        let titel = "titel"
+    document.getElementById(titel).style.color = "red";
+    document.getElementById(titel).innerText = "Inhoud aangepast";
+    
+
+}
---
+++ Opdracht_6.2/app.js
@@ -0,0 +1,6 @@
+function kleur_aanpassen () {
+        let titel = "titel"
+    document.getElementById(titel).style.color = "red";
+    document.getElementById(titel).innerText = "Inhoud aangepast"
+
+}

4298ff873b54ef55af720635920bdb3757b7a87f
Jesse-van-Vliet <9021012@student.zadkine.nl>
2022-10-11 11:14:03 +0200

    7.1

---
+++ index.html/app.js
@@ -0,0 +1,14 @@
+let budget = 100;
+let product = 60;
+
+function   money() {
+if (budget < product) {
+    console.log("Helaas, te weinig geldt");
+
+} else {
+  console.log ("U heeft genoeg geld!") 
+}
+
+}
+
+money()

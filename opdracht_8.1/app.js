870ef65197057f3fbf4537b3eb9dd143c73b4687
Jesse-van-Vliet <9021012@student.zadkine.nl>
2022-11-01 12:00:56 +0100

    opdracht_8.2

--- opdracht_8.1/app.js
+++ opdracht_8.1/app.js
@@ -4,34 +4,60 @@ let h1 = document.getElementById("h1");
 let answer;
 
 function division(){
-    console.log("Het werkt");
-
-    answer = +num1.value / +num2.value;
+    if (num1.value <= 0 || num2.value <= 0) {
+        h1.innerText = "Het getal is te laag"
+    }
+    
+    else {
+        answer = +num1.value / +num2.value;
     h1.innerText = num1.value + " : " + num2.value + " = " + answer;
 
-
+    } 
 }
 
-function plus(){
 
-    answer = +num1.value + +num2.value;
-    h1.innerText = num1.value + " + " + num2.value + " = " + answer;
+function plus(){
+    if (num1.value <= 0 || num2.value <= 0) {
+        h1.innerText = "Het getal is te laag"
+    }
+
+    else  {
+        answer = +num1.value + +num2.value;
+        h1.innerText = num1.value + " + " + num2.value + " = " + answer;
+    }
 
 
 }
 
 function minus(){
 
-    answer = +num1.value - +num2.value;
+    if (num1.value <= 0 || num2.value <= 0) {
+        h1.innerText = "Het getal is te laag"
+    }
+
+    else {
+        answer = +num1.value - +num2.value;
     h1.innerText = num1.value + " - " + num2.value + " = " + answer;
+
+    }
+
+    
 
 
 }
 
 function times(){
 
-    answer = +num1.value * +num2.value;
+    if (num1.value <= 0 || num2.value <= 0) {
+        h1.innerText = "Het getal is te laag"
+    }
+
+    else {
+        answer = +num1.value * +num2.value;
     h1.innerText = num1.value + " x " + num2.value + " = " + answer;
 
+    }
+
+    
 
 }
